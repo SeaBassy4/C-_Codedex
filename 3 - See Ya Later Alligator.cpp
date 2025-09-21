be3e982889b310f33c92789d50d7c49920cb854b
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-09-20 18:31:22 -0700

    3 - See Ya Later Alligator

---
+++ 3 - See Ya Later Alligator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Goodbye in English: \n";
+    std::cout << "Bye!\n";
+    std::cout << "Goodbye in Spanish: \n";
+    std::cout << "Adios!\n";
+    std::cout << "Goodbye in French: \n";
+    std::cout << "Au revoir!\n";
+    std::cout << "Goodbye in German: \n";
+    std::cout << "TschuS!\n";
+}
