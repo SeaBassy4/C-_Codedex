8426d99a75d09b2ee816048b93f947bec03af8e2
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-09-21 17:03:34 -0700

    Create 5 - Sign Bunny.cpp

---
+++ 5 - Sign Bunny.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    std::cout << "|￣￣￣￣￣￣￣|\n";
+    std::cout << "|   HELLO!!   |\n";
+    std::cout << "|    WORLD    |\n";
+    std::cout << "|_____________|\n";
+    std::cout << "(\\__/)" << " ||\n";
+    std::cout << "(•ㅅ•)" << " ||\n";
+    std::cout << "/ 　 づ\n";
+}
