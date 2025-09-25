c881224abbfa991230a41efa3508ad0c69d6f8b9
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-09-25 15:42:56 -0700

    10 - Knighthood

---
+++ 10 - Knighthood.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+
+    std::string name = "Brienne";
+
+    std::cout << "In the name of the Warrior, I charge you to be brave.";
+    std::cout << "In the name of the Father, I charge you to be just.";
+    std::cout << "In the name of the Mother, I charge you to defend the innocent.";
+    std::cout << "Arise, " << name << ", a knight of the Seven Kingdoms.";
+}
