ae0682e693b68e999b08d28a5b6f0a9864b6cfa7
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-09-27 13:59:54 -0700

    14 - Fever

---
+++ 14 - Fever.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int temp = 100;
+
+    if (temp >= 99)
+    {
+        std::cout << "You have a fever.";
+    }
+    else
+    {
+        std::cout << "You don't have a fever";
+    }
+}
