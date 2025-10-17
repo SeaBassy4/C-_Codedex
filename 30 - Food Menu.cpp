64fa77db4a4ec521327ed71202a25cf0c5d8f548
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-10-17 10:48:31 -0700

    30 - Food Menu

---
+++ 30 - Food Menu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void welcome()
+{
+    std::cout << "Welcome to Sonnyboy's Diner! \n";
+    std::cout << " \n";
+    std::cout << "Here's the menu: \n";
+    std::cout << "1. 🍔 Cheeseburger \n";
+    std::cout << "2. 🍟 Fries \n";
+    std::cout << "3. 🥤 Soda \n";
+}
+
+int main()
+{
+    welcome();
+    welcome();
+}
